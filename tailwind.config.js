238b63e595c84afd0b8b9262c1b2c2804b3a16df
Ivan V <ivanntonio@gmail.com>
2024-12-04 23:12:33 -0600

    PaginaAboutMeV02

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
